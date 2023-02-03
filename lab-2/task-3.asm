e76a918206d5ac18a0c4d51fde180eb26686362b
Shanjidul Islam Sadhin <66504343+sadhiin@users.noreply.github.com>
2023-02-04 00:43:34 +0600

    task-3 working

--- lab-2/task-3.asm
+++ lab-2/task-3.asm
@@ -43,3 +43,85 @@ MAIN PROC
     MAIN ENDP
 END MAIN
 RET
+
+
+
+
+
+;-------------------------------NEW----------------------------------
+.model small 
+.stack 100h
+
+.data
+num1 db ?
+num2 db ?  
+msg1 db 'Input first number: $'
+msg2 db 0ah,0dh,'input second number: $'
+res db 0ah,0dh,'Result: $'
+opa db ' + $'
+ops db ' - $'
+opm db ' x $'
+eq db ' = $'
+.code
+
+main proc
+    ; loading the veriables
+    mov ax, @data
+    mov ds, ax
+    
+    ; taking the first number intput
+    mov ah, 9
+    lea dx, msg1
+    int 21h
+    
+    mov ah, 1
+    int 21h 
+    sub al, 48
+    mov num1, al
+ 
+    ; taking second number input 
+    mov ah, 9   
+    lea dx, msg2 
+    int 21h
+    
+    mov ah, 1
+    int 21h 
+    sub al, 48
+    mov num2,al
+    
+    ; Arithmetic oparetion. 
+               
+    mov ah, 9
+    lea dx, res
+    int 21h 
+    
+    ; Additon
+    mov ah,2
+    mov dl, num1
+    int 21h
+    mov ah,9
+    lea dx, opa
+    int 21h
+    mov ah,2
+    mov dl,num2
+    int 21h
+    mov ah,9
+    lea dx,eq
+    int 21h
+     
+ 
+    add num1,num2
+    
+    mov ah,2
+    mov dl, num1
+    int 21h   
+    
+    
+      
+    
+    
+    
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
