6d8883261a00970f1a2bf96c38724ad860a83fcd
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-04-02 14:27:34 +0600

    task 8 done ✅

--- Final/lab-4/task-8.asm
+++ Final/lab-4/task-8.asm
@@ -2,8 +2,9 @@
 .stack 100h
 .data   
 
-msg1 db 10,13,'Task-7$' 
-msg2 db 10,13,'ENTER ANYTHING: $'
+msg1 db 10,13,'Task-8$' 
+msg2 db 10,13,'ENTER ANYTHING: $'  
+msg3 db 10,13,'YOU ENTERED: $'
 newln db 0Ah, 0Dh,"$"
 
 .code
@@ -11,7 +12,8 @@ newln db 0Ah, 0Dh,"$"
 main proc
           
     mov ax,@data
-    mov ds,ax
+    mov ds,ax 
+    
     lea dx,msg1
     mov ah,9                 
     int 21h                  
@@ -20,22 +22,71 @@ main proc
     mov ah, 9
     int 21h
     
-    mov cl, 0
-    jmp input
+    mov ah, 1
+    int 21h 
     
-    input:
+    
+    cmp al, 2
+    jae ifor
+    jmp else
+
+    
+    ifor:
+        ; checking the second condition
+        cmp al,9
+        jle ifcond
+        jmp else
+    ifcond:
+        lea dx, msg2
+        mov ah, 9
+        int 21h
+        
         mov ah,1
-        int 21h  
+        int 21h
+                     
+        mov bl, al
         
-        inc cl 
+        lea dx, msg3
+        mov ah,9
+        int 21h
+        mov ah,2
+        mov dl,bl
+        int 21h 
+        jmp exit 
+    
+    else:
+        ; else condition
+        lea dx,newln
+        mov ah, 9
+        int 21h
         
-        cmp cl,5
-        je exit
+        cmp al,"a"
+        jge charSmall
+        cmp al, "A"
+        jge charCapital
+                     
+    charSmall:
+        cmp al,"z"
+        jle print_C
+        jmp print_N
         
-        jmp input
+    charCapital:   
+        cmp al,"Z"
+        jle print_C
+        jmp print_N 
         
-          
-          
+    print_C:
+        mov ah,2
+        mov dl,"c"
+        int 21h
+        jmp exit
+        
+    print_N:
+        mov ah,2
+        mov dl, "n"
+        int 21h
+        jmp exit
+        
     exit:             
         mov ah,4ch
         int 21h
